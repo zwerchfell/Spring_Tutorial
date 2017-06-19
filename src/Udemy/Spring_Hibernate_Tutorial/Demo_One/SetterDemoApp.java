b532e05eb35d0ad959854b08cb9b355f2781031a
Z Z <zhihao.zou@gmail.com>
2017-06-18 22:35:29 -0400

    Spring Inversion of Control - XML Configuration

---
+++ src/Udemy/Spring_Hibernate_Tutorial/Demo_One/SetterDemoApp.java
@@ -0,0 +1,21 @@
+package Udemy.Spring_Hibernate_Tutorial.Demo_One;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+    public static void main(String[] args) {
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+        // call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+        context.close();
+    }
+}
